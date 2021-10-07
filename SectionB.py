a0900233bc1e866f98d9513fab3a01a6f5f3390b
Garima Gautam <53808998+gari1311@users.noreply.github.com>
2021-10-07 17:23:01 +0530

    Update SectionB.py

--- SectionB.py
+++ SectionB.py
@@ -1,4 +1,6 @@
-# In[1]:
+# Name : Garima Gautam
+# Roll No. : 180001
+# Project 2 Section B
 
 
 # Importing different libraries.
@@ -6,81 +8,35 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
 import warnings
 warnings.filterwarnings('ignore')
+import graphviz 
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score,f1_score
 
-
-# In[2]:
-
-
-# Loading the files.
+# Loading the files
 train=pd.read_csv('../train.csv')
 test=pd.read_csv('../test.csv')
 
 
-# In[4]:
-
-
 test.head()
-
-
-# In[5]:
-
-
 test.tail()
-
-
-# In[6]:
-
-
 test.columns
-
-
-# In[7]:
-
 
 test.dtypes
 
-
-# In[8]:
-
-
 print("Test dataset shape ", test.shape)
 
-
-# In[9]:
-
-
 train.head()
-
-
-# In[10]:
-
-
 train.tail()
-
-
-# In[11]:
-
-
 train.dtypes
-
-
-# In[12]:
-
 
 print("Train dataset shape ", train.shape)
 
-
-# In[13]:
-
-
 # Checking for missing vales and fix them appropriately.
 train.isnull().sum()
-
-
-# In[14]:
 
 
 train["Gender"].fillna(train["Gender"].mode()[0],inplace=True)
@@ -89,45 +45,17 @@ train['Dependents'].fillna(train["Dependents"].mode()[0],inplace=True)
 train["Self_Employed"].fillna(train["Self_Employed"].mode()[0],inplace=True)
 train["Credit_History"].fillna(train["Credit_History"].mode()[0],inplace=True)
 
-
-# In[15]:
-
-
 train["Loan_Amount_Term"].value_counts()
-
-
-# In[16]:
-
 
 train["Loan_Amount_Term"].fillna(train["Loan_Amount_Term"].mode()[0],inplace=True)
 
-
-# In[17]:
-
-
 train["Loan_Amount_Term"].value_counts()
-
-
-# In[18]:
-
 
 train["LoanAmount"].fillna(train["LoanAmount"].median(),inplace=True)
 
-
-# In[19]:
-
-
 train.isnull().sum()
 
-
-# In[20]:
-
-
 test.isnull().sum()
-
-
-# In[21]:
-
 
 test["Gender"].fillna(test["Gender"].mode()[0],inplace=True)
 test['Dependents'].fillna(test["Dependents"].mode()[0],inplace=True)
@@ -136,69 +64,29 @@ test["Loan_Amount_Term"].fillna(test["Loan_Amount_Term"].mode()[0],inplace=True)
 test["Credit_History"].fillna(test["Credit_History"].mode()[0],inplace=True)
 test["LoanAmount"].fillna(test["LoanAmount"].median(),inplace=True)
 
-
-# In[22]:
-
-
 test.isnull().sum()
-
-
-# In[23]:
-
-
 train.info()
-
-
-# In[50]:
-
 
 test.info()
 
-
-# In[52]:
-
-
-#### Encoding categrical Features: ##########
+# Encoding categrical Features: 
 train_encoded = pd.get_dummies(train,drop_first=True)
 train_encoded.head()
 
-
-# In[54]:
-
-
-########## Split Features and Target Varible ############
+# Split Features and Target Varible
 X = train_encoded.drop(columns='Loan_Status_Y')
 y = train_encoded['Loan_Status_Y']
 
-
-# In[55]:
-
-
-################# Splitting into Train -Test Data #######
+# Splitting into Train -Test Data
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,stratify =y,random_state =42)
 
-
-# In[56]:
-
-
-############### Handling/Imputing Missing values #############
+# Handling/Imputing Missing values
 from sklearn.impute import SimpleImputer
 imp = SimpleImputer(strategy='mean')
 imp_train = imp.fit(X_train)
 X_train = imp_train.transform(X_train)
 X_test_imp = imp_train.transform(X_test)
-
-
-# In[57]:
-
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score,f1_score
-
-
-# In[58]:
 
 
 tree_clf = DecisionTreeClassifier()
@@ -209,10 +97,6 @@ print("Training Data F1 Score ", f1_score(y_train,y_pred))
 
 print("Validation Mean F1 Score: ",cross_val_score(tree_clf,X_train,y_train,cv=5,scoring='f1_macro').mean())
 print("Validation Mean Accuracy: ",cross_val_score(tree_clf,X_train,y_train,cv=5,scoring='accuracy').mean())
-
-
-# In[59]:
-
 
 training_accuracy = []
 val_accuracy = []
@@ -245,21 +129,12 @@ fig,ax = plt.subplots(figsize=(15,5))
 sns.pointplot(x="Max_Depth", y="Values",hue="Metrics", data=plot_df,ax=ax)
 
 
-# In[62]:
-
-
-import graphviz 
-from sklearn import tree
 
 tree_clf = tree.DecisionTreeClassifier(max_depth = 3)
 tree_clf.fit(X_train,y_train)
 dot_data = tree.export_graphviz(tree_clf,feature_names = X.columns.tolist())
 graph = graphviz.Source(dot_data)
 graph
-
-
-# In[63]:
-
 
 training_accuracy = []
 val_accuracy = []
@@ -292,9 +167,6 @@ fig,ax = plt.subplots(figsize=(15,5))
 sns.pointplot(x="Min_Samples_leaf", y="Values",hue="Metrics", data=plot_df,ax=ax)
 
 
-# In[64]:
-
-
 from sklearn.metrics import confusion_matrix
 tree_clf = DecisionTreeClassifier(max_depth=3,min_samples_leaf = 35)
 tree_clf.fit(X_train,y_train)
@@ -303,10 +175,3 @@ print("Test Accuracy: ",accuracy_score(y_test,y_pred))
 print("Test F1 Score: ",f1_score(y_test,y_pred))
 print("Confusion Matrix on Test Data")
 pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'], margins=True)
-
-
-# In[ ]:
-
-
-
-
